1d8c07b19708e5b3b8c7f48ba1e0c0b89a6cb8f8
Josh Westbury <joshwestbury@Joshs-MacBook-Pro.local>
2018-08-18 13:45:06 -0500

    add input to search_bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // import the React library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 // Create a new component that produces some HTML.
 const App = () => {
-    return <div>Hi</div>; //this HTML is acutally JSX. JSX is a subset of JS that allows us to write what looks
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    ); //this HTML is acutally JSX. JSX is a subset of JS that allows us to write what looks
                            // like HTML but is actually JS.
 }
 
