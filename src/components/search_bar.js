1d061790ca10020f060e64afe8f6875c4654b082
Josh Westbury <joshwestbury@Joshs-MBP.attlocal.net>
2018-08-19 07:42:33 -0500

    add constructor and initialized state to search_bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component { 
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''}
+    }
     render() { 
         return <input onChange={e => console.log(e.target.value)}/>
     }  
