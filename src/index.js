4c1f7dab1a04d3505de5490411a0eee321214129
Josh Westbury <joshwestbury@Joshs-MBP.attlocal.net>
2018-08-19 16:35:50 -0500

    implement youtube api call

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import { API_KEY } from '../config';
 
 // Create a new component that produces some HTML.
-
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 const App = () => {
     return (
         <div>
