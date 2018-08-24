5183f4c600f7d75f564c859735c8a0b5b65bed14
Josh Westbury <joshwestbury@Joshs-MBP.attlocal.net>
2018-08-24 06:53:56 -0500

    add search bar functionality

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -7,15 +7,16 @@
  const VideoList = (props) => {
     const videoItems = props.videos.map(video => {
         return (
-        <VideoListItem 
-            key={video.etag} 
-            video={video} 
-            onVideoSelect={props.onVideoSelect} />
+            <VideoListItem 
+                key={video.etag} 
+                video={video} 
+                onVideoSelect={props.onVideoSelect} 
+            />
         )
     })
 
     return (
-        <ul className="col-md-12 list-group">
+        <ul className="col-md-4 list-group">
             {videoItems}
         </ul>
     );
--- src/index.js
+++ src/index.js
@@ -17,7 +17,12 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'fails'}, (videos) => {
+        this.videoSearch('surfboards');
+       
+    }
+
+    videoSearch(term) { //term is going to be search term
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0] //so we always play the first video in search result
@@ -28,7 +33,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                 onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
