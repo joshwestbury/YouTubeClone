3b6afcbf4c0d612e31193bf823b620e1ea5b08e4
Josh Westbury <joshwestbury@Joshs-MBP.attlocal.net>
2018-08-25 09:16:20 -0500

    throttle search input with lodash debounce method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
@@ -31,9 +32,12 @@ class App extends Component {
     }
 
     render() {
+
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                 onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
